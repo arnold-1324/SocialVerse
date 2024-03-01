15f8fa4a07d44b60c322a34e991d1c4f95793674
arnold-1324 <141809292+arnold-1324@users.noreply.github.com>
2024-03-01 09:33:22 +0530

    first comment

---
+++ src/pages/About.jsx
@@ -0,0 +1,25 @@
+
+
+const About = () => {
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm <span className="blue-gradient_text font-semibold
+        drop-shadow">Dhinesh</span></h1>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>Software Developer based in India, specialized in
+          technical education throught hands-on learning and building
+          applications.  
+          </p>
+        </div>
+        <div className="py-10 flex flex-col">
+         <h3 className="subhead-text"></h3>
+         <div className="mt-16 flex flex-wrap gap-12">
+         
+         </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
